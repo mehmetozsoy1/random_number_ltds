93c9e782e536570f8fe7e2ddeeca93c1b3258862
Mehmet Oktay Özsoy <mehmetoktayozsoy@Mehmets-MacBook-Pro-2.local>
2024-01-02 23:30:04 +0300

    pyproject.toml added

--- src/random_number_ltds/random_number_generator.py
+++ src/random_number_ltds/random_number_generator.py
@@ -16,4 +16,6 @@ def return_random_dict(lower_bound: int = 0,
 def return_random_set(lower_bound: int = 0, 
                        upper_bound: int = 6):
     return {return_random_number(lower_bound, upper_bound) for _ in return_random_number(lower_bound, upper_bound)}
+
+...
     
